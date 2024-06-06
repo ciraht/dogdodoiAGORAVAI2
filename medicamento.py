252ea95aab337efd9fdc9fab8afc9447898ba0a7
ciraht <tharictalon@gmail.com>
2024-06-06 10:40:43 -0300

    Rota para calcular a miligramas/peso corporal do medicamento

--- medicamento.py
+++ medicamento.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+@app.route('/medicamento', methods=['GET'])
+def mostrar_formulario():
+    return render_template('medicamento.html', volume_total='', dose='')
 @app.route('/medicamento', methods=['POST'])
 def calcular_soro():
     grau_desidratacao = request.form.get('grau_desidratacao')
@@ -14,16 +18,51 @@ def calcular_soro():
     elif grau_desidratacao == 'Grave':
         volume_fluidoterapia = 100
     else:
-        return 'Grau de desidratação inválido'
+        volume_fluidoterapia = 0
 
     volume_total = volume_fluidoterapia * peso_animal
 
-    #return f'O volume total de soro necessário é: {volume_total} ml/kg'
     return render_template('medicamento.html', volume_total=volume_total)
 
-@app.route('/medicamento')
-def mostrar_formulario():
-    return render_template('medicamento.html', volume_total='')
+# @app.route('/medicamento')
+# def mostrar_formulario():
+#     return render_template('medicamento.html', volume_total='')
+
+@app.route('/dose', methods=['POST'])
+def medida_medicamento():
+    medicamento = request.form.get('remedio')
+    peso_animal2 = float(request.form.get('peso_animal2'))
+
+    if medicamento == 'Petrilan':
+        valor = 5
+    elif medicamento == 'Canidexa':
+        valor = 10
+    elif medicamento == 'Felinoflam':
+        valor = 3
+    elif medicamento == 'Puprilon':
+        valor = 7
+    elif medicamento == 'Meowmed':
+        valor = 4
+    elif medicamento == 'Vetranix':
+        valor = 6
+    elif medicamento == 'Caniforte':
+        valor = 8
+    elif medicamento == 'FeliCure':
+        valor = 5
+    elif medicamento == 'Pawsitrex':
+        valor = 4.5
+    elif medicamento == 'WoofGuard':
+        valor = 9
+    elif medicamento == 'Outro':
+        valor = float(request.form.get('outros'))
+    else:
+        return 'Medicamento inválido'
+
+    dose = valor * peso_animal2
+
+    return render_template('medicamento.html', dose=dose)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
